191b2e7fea48c1ee24e93f38c4ebee5adc60518b
Andreea Taflan <andreea07.05.2001@gmail.com>
2025-11-13 17:26:25 +0100

    footer add

---
+++ my-react-app/src/pages/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//import "../style/Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
