ea99d4ff9c6b99c5453721a915276841ceee1b82
Andreea Taflan <andreea07.05.2001@gmail.com>
2025-12-22 21:17:36 +0100

    adding profile page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Footer from "./components/Footer";
 import Header from './components/header';
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile/>} />
       </Routes>
       <Footer/>
     </>
