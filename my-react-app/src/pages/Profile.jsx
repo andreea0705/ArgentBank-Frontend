825bec4c227833b8beca803564737c821cbaa7d3
Andreea Taflan <andreea07.05.2001@gmail.com>
2025-12-22 21:36:31 +0100

    adding le contenu

--- my-react-app/src/pages/Profile.jsx
+++ my-react-app/src/pages/Profile.jsx
@@ -3,7 +3,43 @@ import React from "react";
 
 const Profile =() => {
     return (
-        <h1>Hello</h1>
+         <main classname="main bg-dark">
+      <div classname="header">
+        <h1>Welcome back<br />Tony Jarvis!</h1>
+        <button classname="edit-button">Edit Name</button>
+      </div>
+      <h2 classname="sr-only">Accounts</h2>
+      <section classname="account">
+        <div classname="account-content-wrapper">
+          <h3 classname="account-title">Argent Bank Checking (x8349)</h3>
+          <p classname="account-amount">$2,082.79</p>
+          <p classname="account-amount-description">Available Balance</p>
+        </div>
+        <div classname="account-content-wrapper cta">
+          <button classname="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section classname="account">
+        <div classname="account-content-wrapper">
+          <h3 classname="account-title">Argent Bank Savings (x6712)</h3>
+          <p classname="account-amount">$10,928.42</p>
+          <p classname="account-amount-description">Available Balance</p>
+        </div>
+        <div classname="account-content-wrapper cta">
+          <button classname="transaction-button">View transactions</button>
+        </div>
+      </section>
+      <section classname="account">
+        <div classname="account-content-wrapper">
+          <h3 classname="account-title">Argent Bank Credit Card (x8349)</h3>
+          <p classname="account-amount">$184.30</p>
+          <p classname="account-amount-description">Current Balance</p>
+        </div>
+        <div classname="account-content-wrapper cta">
+          <button classname="transaction-button">View transactions</button>
+        </div>
+      </section>
+    </main>
     )
 }
 
